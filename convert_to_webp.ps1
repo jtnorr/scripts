1f20f544d018488a9ea604c58c97e485a2161001
Jani Norrby <jani.t.norrby@utu.fi>
2024-07-13 18:17:37 +0300

    Refactor script to improve directory handling, add audio normalization option

--- convert_to_webp.ps1
+++ convert_to_webp.ps1
@@ -2,18 +2,40 @@
 # download here: https://developers.google.com/speed/webp/download
 
 Param (
-    [string]$Directory = 'C:\Users\janin\OneDrive\Desktop',
-    [string]$DestinationPath = 'C:\Users\janin\OneDrive\Desktop\converted',
-    [string]$Quality = '75',
-    [string]$Preset
+    [ValidateScript({Test-Path $_ -PathType Container})]
+    [string]$Directory = '.',
+
+    [ValidateScript({Test-Path $_ -PathType Container})]
+    [string]$DestinationPath = '.\Converted',
+
+    [ValidateRange(1, 100)]
+    [int]$Quality = 75,
+
+    [ValidateSet('default', 'photo', 'picture', 'drawing', 'icon', 'text')]
+    [string]$Preset = 'default'
 )
 
-if ([string]::IsNullOrEmpty($Preset)) {
-    $Preset = Read-Host "Enter the Preset type (default, photo, picture, drawing, icon, text)"
-}
+
 
 if (-not (Test-Path $Directory -PathType Container)) {
     Write-Host "Directory not found."
     Exit
 }
 
+if (-not (Test-Path $DestinationPath -PathType Container)) {
+    New-Item -Path $DestinationPath -ItemType Directory | Out-Null
+    Write-Host "Destination directory created at $DestinationPath."
+}
+
+Get-ChildItem -Path $Directory -Include "*.png", "*.jpg" , "*.gif" -File | ForEach-Object {
+    $sourceFile = $_.FullName
+    $destinationFile = Join-Path -Path $DestinationPath -ChildPath ($_.BaseName + '.webp')
+    $args = "-q $Quality -preset $Preset `"$sourceFile`" -o `"$destinationFile`""
+    
+
+}
+
+
+Param (
+
+)
